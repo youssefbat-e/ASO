ccc04651a8439f616b55bbd94eafaea0a346ea21
ivetMompin <ivet.mompin@students.salle.url.edu>
2023-11-30 10:08:41 +0100

    process mgmnt current

--- scripts/processMgmnt.sh
+++ scripts/processMgmnt.sh
@@ -4,6 +4,8 @@ echo "Content-type: text/html"
 echo
 read -p "Enter PID: " pid
 pidCorrect="${pid:4}"
+read -p "Enter STATUS: " status
+echo "$status"
 outputPs=$(ps -aux)
 header=$(echo "$outputPs" | head -n 1)
 outputPs=$(echo "$outputPs" | tail -n +2)
@@ -17,22 +19,9 @@ if [ -n "$statusCorrect" ]; then
     outputFinal=$(echo "$outputPs" | awk -v status="$statusCorrect" '$10 == status')
     outputPs=$outputFinal
 fi
-# HTML header
-echo "<!DOCTYPE html>"
-echo "<html lang='en'>"
-echo "<head>"
-echo "    <meta charset='UTF-8'>"
-echo "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>"
-echo "    <title>Process Info</title>"
-echo "</head>"
-echo "<br></br>"
-echo "<form action="/scripts/processMgmnt.sh" method="post">"
-echo "      PID: <input type="text" name="pid" /></br />"
-echo "      <input type="submit" value="Search" />"
-echo "</form>"
-echo "<br></br>"
-function PrintTable {
-    echo "<table border='1'>"
+
+PrintTable() {
+    echo "<table>"
     echo "<tr>"
     for ((j = 1; j <= 11; j++)); do
             column=$(echo "$header" | awk -v col=$j '{print $col}')
@@ -50,8 +39,23 @@ function PrintTable {
 echo "</table>"
 }
 
+# HTML header
+echo "<!DOCTYPE html>"
+echo "<html lang='en'>"
+echo "<head>"
+echo "    <meta charset='UTF-8'>"
+echo "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+echo "    <title>Process Info</title>"
+echo "</head>"
 echo "<body>"
 echo "<h1>PROCESSES INFO</h1>"
+echo "<br></br>"
+echo "<form action="/scripts/processMgmnt.sh" method="post">"
+echo "      PID: <input type="text" name="pid" /></br />"
+echo "      STATUS: <input type="text" name="status" /></br />"
+echo "      <input type="submit" value="Search" />"
+echo "</form>"
+echo "<br></br>"
 if [ -n "$outputPs" ]; then
 echo "<table border='1'>"
 PrintTable
